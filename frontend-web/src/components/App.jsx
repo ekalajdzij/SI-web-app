b75b235b0e1c5f1c238c559ac75ea1949531d9a8
slicina7 <slicina1@etf.unsa.ba>
2024-03-24 18:01:56 +0100

    Login and registration UI

--- frontend-web/src/components/App.jsx
+++ frontend-web/src/components/App.jsx
@@ -1,23 +1,19 @@
-import { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import '../css/App.css'
-import Login from './Login';
-import Register from './Register';
+import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "../css/App.css";
+import Login from "./Login";
+import Register from "./Register";
 
-import Home from './Home'; // Pretpostavljamo da imate komponentu RouteMap
+import Home from "./Home"; // Pretpostavljamo da imate komponentu RouteMap
 
 function App() {
   return (
     <Router>
-      
-        <Routes>
-
+      <Routes>
         <Route path="/home" element={<Home />} />
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
-
-        </Routes>
-      
+      </Routes>
     </Router>
   );
 }
