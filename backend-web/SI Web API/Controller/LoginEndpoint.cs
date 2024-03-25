de1c7bbd565e0cfde8ce26ec63f63cad4b3ace8a
Emir <ekalajdzij1@etf.unsa.ba>
2024-03-25 09:53:02 +0100

    implemented 2fa and minor fixes to login

--- backend-web/SI Web API/Controller/LoginEndpoint.cs
+++ backend-web/SI Web API/Controller/LoginEndpoint.cs
@@ -6,13 +6,19 @@ using Microsoft.AspNetCore.Routing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages.Manage;
+using QRCoder;
 using SI_Web_API.Data;
+using SI_Web_API.Dtos;
 using SI_Web_API.Model;
 using SI_Web_API.Services;
+using System.Drawing;
 using System.Globalization;
 using System.IdentityModel.Tokens.Jwt;
 using System.Text;
 using System.Text.Json;
+using System.Text.RegularExpressions;
+using System.IO;
 
 namespace SI_Web_API.Controller;
 
@@ -24,21 +30,54 @@ public static class LoginEndpoints
 
         var group = routes.MapGroup("/api/login").WithTags(nameof(User));
 
-        group.MapPost("/", async (HttpContext httpContext, [FromBody] User payload, SI_Web_APIContext db) =>
+        group.MapPost("/", async (HttpContext httpContext, [FromBody] LoginRequest payload, SI_Web_APIContext db) =>
         {
-            var user = await db.User.FirstOrDefaultAsync(u => u.Username == payload.Username &&
+            var user = await db.User.FirstOrDefaultAsync(u => (u.Username == payload.Username || u.PhoneNumber == payload.Username) &&
                 u.Password == payload.Password);
+
             if (user == null) return Results.NotFound("User not found.");
             else
             {
-                payload.FullName = user.FullName;
-                payload.Token = AuthService.GenerateJwtToken(issuer, key); ;
-                payload.Role = user.Role;
-                payload.Mail = user.Mail;
-                return Results.Ok(payload);
+                user.Token = AuthService.GenerateJwtToken(issuer, key);
+                return Results.Ok(user);
             }
         }).WithName("GetUserByUsernameAndPassword")
         .AllowAnonymous()
         .WithOpenApi();
+
+        group.MapPost("/setup/2fa", async (HttpContext httpContext, SI_Web_APIContext db, [FromBody] LoginRequest payload) =>
+        {
+            var user = await db.User.FirstOrDefaultAsync(u => (u.Username == payload.Username || u.PhoneNumber == payload.Username) &&
+                u.Password == payload.Password);
+            if (user == null) return Results.NotFound("User not found.");
+            else
+            {
+                var setup2fa = TwoFactorAuthService.GenerateSetupCode(user, db);
+                return Results.Ok(new
+                {
+                    ManualEntryKey = setup2fa.ManualEntryKey,
+                    QRCodeImageUrl = $"https://chart.googleapis.com/chart?cht=qr&chs=200x200&chl=otpauth://totp/SIWeb%20App:{Uri.EscapeDataString(user.FullName)}%3Fsecret={setup2fa.ManualEntryKey}%26issuer=SIWeb%20App"
+                });
+            }
+        }).WithName("SetupTwoFactorAuth");
+
+
+        group.MapPost("/authenticate/2fa", async (HttpContext httpContext, string code, SI_Web_APIContext db, [FromBody] LoginRequest payload) =>
+        {
+            var user = await db.User.FirstOrDefaultAsync(u => (u.Username == payload.Username || u.PhoneNumber == payload.Username) &&
+                 u.Password == payload.Password);
+            if (user == null) return Results.NotFound("User not found.");
+            else
+            {
+                bool isValidToken = TwoFactorAuthService.ValidateToken(user.SecretKey, code);
+                if (!isValidToken) return Results.BadRequest("Invalid code. Please try again.");
+                else
+                {
+                    return Results.Ok();
+                }
+            }
+        }).WithName("AuthorizeToken");
+
+
     }
 }
--- backend-web/SI Web API/Model/User.cs
+++ backend-web/SI Web API/Model/User.cs
@@ -9,10 +9,13 @@ namespace SI_Web_API.Model
         public int Id { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
+        public string PhoneNumber { get; set; }
         public string FullName { get; set; }
         public string Mail { get; set; }
         [NotMapped]
         public string Token { get; set; }
         public string Role { get; set; }
+        public string SecretKey { get; set; }
+
     }
 }
