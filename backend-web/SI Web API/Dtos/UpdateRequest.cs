0b27acd19aff11aac9b2a0160edf6b965a6920c9
Emir <ekalajdzij1@etf.unsa.ba>
2024-03-31 13:32:10 +0200

    UserCampaign and Campaign routes

---
+++ backend-web/SI Web API/Dtos/UpdateRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace SI_Web_API.Dtos
+{
+    public class UpdateRequest
+    {
+        [Required]
+        public int? UserId { get; set; }
+        [Required]
+        public int? CampaignId { get; set; }
+        [Required]
+        public string? Status { get; set; }
+    }
+}
