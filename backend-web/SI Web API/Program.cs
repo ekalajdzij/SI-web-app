0860f22f720f5fe42e4bf7a2c9ef6d1dceb83e97
Griza20 <agrizovic1@etf.unsa.ba>
2024-04-28 17:52:19 +0200

    add new endpoints

--- backend-web/SI Web API/Program.cs
+++ backend-web/SI Web API/Program.cs
@@ -87,12 +87,8 @@ builder.Services.AddSwaggerGen(c =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseCors("AllowAll");
 
