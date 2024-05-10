4df4995218411dc440478e5ca8d5091627b8fd56
ndzajic1 <ndzajic1@etf.unsa.ba>
2024-05-10 09:07:19 +0200

    Fixed docker installation

--- install.sh
+++ install.sh
@@ -8,8 +8,14 @@ if [ -x "$(command -v docker)" ]; then
 else
     echo "Installing docker..."
     sudo apt-get update
-    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
-    sudo systemctl start docker
+    sudo apt-get install curl 
+    sudo apt install apt-transport-https ca-certificates curl software-properties-common
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt update
+    sudo apt install docker-ce -y
+
+    sudo systemctl status docker
 fi
 
 if [ -x "$(command -v docker-compose)" ]; then
