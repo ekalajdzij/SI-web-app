275567433d8eda7ed70ef80833a1fce97d5effbf
tvelic1 <92871186+tvelic1@users.noreply.github.com>
2024-05-31 18:23:43 +0200

    Update Map.jsx

--- frontend-web/src/components/Map.jsx
+++ frontend-web/src/components/Map.jsx
@@ -11,7 +11,7 @@ import html2canvas from "html2canvas";
 import jsPDF from "jspdf";
 import { useNavigate } from "react-router-dom";
 import "../css/map.css";
-const googleMapsApiKey = "AIzaSyAnoTrrumleEp9aG0CudXZPdHdey1Fn3R0";
+const googleMapsApiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
 
 const mapContainerStyle = {
   width: "1000px",
