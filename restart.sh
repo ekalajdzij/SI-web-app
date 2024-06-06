81117479fe4975370d17f78ff1af497d45d7f13e
ndzajic1 <ndzajic1@etf.unsa.ba>
2024-06-06 19:46:26 +0200

    Modify restart script for purge

--- restart.sh
+++ restart.sh
@@ -1,7 +1,6 @@
 docker stop si-web-app_react_frontend_1
 docker stop si-web-app_dotnet_backend_1
-docker stop si-web-app_mssql_1
+docker stop mysql-db
 
-docker rm si-web-app_react_frontend_1
-docker rm si-web-app_dotnet_backend_1
-docker rm si-web-app_mssql_1
+docker system prune -a --volumes
+
