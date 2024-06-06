85bae2cabf4983fed1546bcd80fc82580960d5e7
ndzajic1 <ndzajic1@etf.unsa.ba>
2024-06-07 00:03:44 +0200

    Manual running containers

--- install.sh
+++ install.sh
@@ -27,11 +27,18 @@ else
 fi
 
 sudo docker-compose build
-sudo docker-compose up -d
+# sudo docker-compose up -d
+
+# Manual run
+sudo docker run --name mysql-db -e MYSQL_DATABASE=baza -e MYSQL_ROOT_PASSWORD=password -d mysql:latest
+sleep 5
+sudo docker run -dp 5200:8080 --name back -e REACT_APP_BACKEND_URL=http://localhost:8080/  -e ASPNETCORE_ENVIRONMENT=Development -d si-web-app_dotnet_backend
+sudo docker run -dp 5173:5173 --name front -d si-web-app_react_frontend
+
 echo "Images are built..."
 
-sudo docker cp dbinit.sql mysql-db:dbinit.sql
-sudo docker exec -u root mysql-db sh -c "mysql -u root -p password  baza < dbinit.sql"
+#sudo docker cp dbinit.sql mysql-db:dbinit.sql
+#sudo docker exec -u root mysql-db sh -c "mysql -u root -p password  baza < dbinit.sql"
 
 
 
