57fbb2d982331be02a24b10d44fe0952041e47e4
Nedim Džajić <115584541+ndzajic1@users.noreply.github.com>
2024-06-09 12:21:57 +0200

    empty db first

--- dbinit.sql
+++ dbinit.sql
@@ -1,3 +1,12 @@
+DELETE FROM Record;
+DELETE FROM LocationStatus;
+DELETE FROM UserCampaign;
+DELETE FROM Location;
+DELETE FROM Campaign;
+DELETE FROM User;
+DELETE FROM Admin;
+DELETE FROM Company;
+
 INSERT INTO Admin
 (Id, Username, Password, IsSuperAdmin, CompanyId, PhoneNumber, SecretKey)
 VALUES(4, N'superkeno', N'9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08', 1, NULL, N'09090', N'');
