b06c11eb4bcf7a0fba489b0275b7ac07557aca6c
Nedim Džajić <115584541+ndzajic1@users.noreply.github.com>
2024-06-09 14:48:46 +0200

    New passwords in  seed script

--- dbinit.sql
+++ dbinit.sql
@@ -9,13 +9,13 @@ DELETE FROM Company;
 
 INSERT INTO Admin
 (Id, Username, Password, IsSuperAdmin, CompanyId, PhoneNumber, SecretKey)
-VALUES(4, N'superkeno', N'9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08', 1, NULL, N'09090', N'');
+VALUES(4, N'superkeno', N'superkeno', 1, NULL, N'09090', N'');
 
 INSERT INTO Admin
 (Id, Username, Password, IsSuperAdmin, CompanyId, PhoneNumber, SecretKey)
-VALUES(35, N'vela', N'9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08', 1, NULL, N'564', N'');
+VALUES(35, N'vela', N'vela', 1, NULL, N'56477', N'');
 
 INSERT INTO Admin
 (Id, Username, Password, IsSuperAdmin, CompanyId, PhoneNumber, SecretKey)
-VALUES(87, N'dzajicad', N'9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08', 1, NULL, N'11111111', N'');
+VALUES(87, N'dzajic', N'dzajic', 1, NULL, N'11111111', N'');
 
